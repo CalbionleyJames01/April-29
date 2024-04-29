0081bd11b551a45c801b4479dc3efb6b069c561d
CalbionleyJames01 <134080933+CalbionleyJames01@users.noreply.github.com>
2024-04-29 14:06:40 +0800

    Add files via upload

---
+++ Monday/Activity1.java
@@ -0,0 +1,14 @@
+package April.Monday;
+
+public class Activity1 {
+    public static void main(String[] args) {
+        String originalString = "Java Exercises!";
+
+        char charAtIndex0 = originalString.charAt(0);
+        System.out.println("Original String = " + originalString);
+        System.out.println("The character at position 0 is " + charAtIndex0);
+
+        char charAtIndex10 = originalString.charAt(10);
+        System.out.println("The character at position 10 is " + charAtIndex10);
+    }
+}
---
+++ Monday/Activity3.java
@@ -0,0 +1,12 @@
+package April.Monday;
+
+public class Activity3 {
+    public static void main(String[] args) {
+        String str1 = "PHP Exercises and ";
+        String str2 = "Python Exercises";
+        String concatenatedString = str1 + str2;
+        System.out.println("String 1: " + str1);
+        System.out.println("String 2: " + str2);
+        System.out.println("The concatenated string: " + concatenatedString);
+    }
+}
